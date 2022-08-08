7aa03fd6b8f49293ebaaa65011d03a6d5ffb45f4
Engine Bai <enginebai@gmail.com>
2022-08-08 13:26:38 +0800

    Add components compose library module

---
+++ components/src/main/java/com/funnow/core/InputFields.kt
@@ -0,0 +1,10 @@
+package com.funnow.core
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun Greeting(title: String) {
+    Text(title, fontSize = 32.sp)
+}
